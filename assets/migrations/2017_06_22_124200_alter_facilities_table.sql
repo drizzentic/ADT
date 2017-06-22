6047c4690a801d092a4ce15077729c9de5c8d098
Nguru <billnguts@gmail.com>
2017-06-22 14:32:45 +0300

    migrations

---
+++ assets/migrations/2017_06_22_124200_alter_facilities_table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE facilities
+ADD autobackup tinyint(1) NOT NULL DEFAULT '0' AFTER map//
+
+ALTER TABLE facilities DROP lost_to_follow_up //
+
+ALTER TABLE facilities ADD lost_to_follow_up int(11) default 90 AFTER autobackup//
