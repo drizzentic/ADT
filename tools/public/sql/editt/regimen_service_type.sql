78161d3baef0f3da469c28eed418e1c6277e4506
Nguru <billnguts@gmail.com>
2017-07-07 17:46:25 +0300

    publick --> /assets/public

--- tools/public/sql/editt/regimen_service_type.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	name,
-	1 AS active,
-	{destination_store_id} AS ccc_store_sp
-FROM service_type
-WHERE {migration_flag_column} = {migration_flag_default}
-LIMIT {migration_limit}
-OFFSET {migration_offset}//
