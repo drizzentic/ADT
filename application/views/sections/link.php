db37044e13b34566c6ea608661f8ac41a3e0de6c
Kevin Marete <kevomarete@gmail.com>
2017-07-20 13:34:48 +0300

    fixed untracked files

--- application/views/sections/link.php
+++
@@ -1,2 +0,0 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14992689011cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1496910962ca1b943a45a1aca524c9fc3e8a2147ff.js" charset="UTF-8"></script>
