53dd3f4e894d5bab89055ba9e83f19188cb79914
Nguru <billnguts@gmail.com>
2017-11-21 16:36:05 +0300

    backup archiving without including directories

--- tools/application/modules/backup/controllers/backup.php
+++ tools/application/modules/backup/controllers/backup.php
@@ -11,11 +11,12 @@ class Backup extends MX_Controller {
 		'password' => 'ftpuser',
 		'debug'	=> FALSE);
 	var $ftp_root = '/backups/';
+	var $key = "top secret key09";
 
 	function __construct() {
 		parent::__construct();
 		$this->load->library('ftp');
-	    $this->load->library('session');
+		$this->load->library('session');
 
 	}
 
@@ -236,9 +237,7 @@ class Backup extends MX_Controller {
 
 
 	public function run_backup() {
-
-		// $file_path = $this -> input -> post('location', TRUE);
-		$file_path =  FCPATH.'backup_db';
+		$file_path =  FCPATH;
 
 		$file_path = addslashes($file_path);
 		$CI = &get_instance();
@@ -269,14 +268,14 @@ class Backup extends MX_Controller {
 			$file_path = "\"" . $file_path . "//" . $outer_file . "\"";
 			$mysql_bin = str_replace("//", "////", $mysql_home);
 			$mysql_con = $mysql_bin.'' . ' -u ' . $username . ' -p' . $password . ' -h ' . $hostname . ' -P '.$port.' '. $current_db . ' > ' . $file_path;
+			
 			exec($mysql_con);
 			$error_message = "<div class='alert-success'><button type='button' class='close' data-dismiss='alert'>&times;</button><strong>Backup!</strong> Database Backup Successful. $outer_file </div>";
 			$this -> session -> set_flashdata('error_message', $error_message);
-			// redirect("backup_management");
-			// echo str_replace('"', "", $file_path);
-			if($this->zip_backup(str_replace('"', "", $file_path))){
+			if($this->zip_backup($outer_file, str_replace('"', "", $file_path))){
 
 				$this->delete_file(str_replace('"', "", $file_path));
+
 				echo "Backup Success - " . $outer_file.'.zip';die;
 
 			}
@@ -296,13 +295,13 @@ class Backup extends MX_Controller {
 		$result = $CI->db->query($sql);
 		$facility_code = $result->result_array()[0]['Facility_Code']; 
 
-			$list = $this->ftp->list_files($this->ftp_root.'/');
+		$list = $this->ftp->list_files($this->ftp_root.'/');
 
-			if (!in_array($this->ftp_root.'/'.$facility_code.'', $list)){
-				$this->ftp->mkdir($this->ftp_root.$facility_code.'/', 0755);
-			}
-			$uploaded_backups = $this->ftp->list_files($this->ftp_root.$facility_code.'/');
-			return $uploaded_backups;
+		if (!in_array($this->ftp_root.'/'.$facility_code.'', $list)){
+			$this->ftp->mkdir($this->ftp_root.$facility_code.'/', 0755);
+		}
+		$uploaded_backups = $this->ftp->list_files($this->ftp_root.$facility_code.'/');
+		return $uploaded_backups;
 	}
 
 	public function download_remote_file($remote_path = null){
@@ -320,11 +319,17 @@ class Backup extends MX_Controller {
 	}
 
 	public function upload_backup($file_name = null) {
-		// $file_name =$_POST['file_name'];
+
+
+		ini_set('upload_max_filesize', '10M');
+		ini_set('post_max_size', '10M');
+		ini_set('max_input_time', 300);
+		ini_set('max_execution_time', 300);
+		set_time_limit(0);
+
+
 		$file_name = (isset($file_name)) ? $file_name : $_POST['file_name'] ;
 		$file_path =  FCPATH.'backup_db/'.$file_name;
-		// echo $file_path." fp";die;
-
 
 		$this->connect_ftp();
 
@@ -344,15 +349,61 @@ class Backup extends MX_Controller {
 		$uploaded_backups = $this->ftp->list_files($this->ftp_root.$facility_code.'/');
 		
 		if (!in_array($this->ftp_root.$facility_code.'/'.$file_name, $uploaded_backups)){
-			$this->ftp->upload($file_path,$this->ftp_root.$facility_code.'/'.$file_name, 'ascii', 0775);
-			echo "Upload  Successful";
+			// $this->ftp->upload($file_path,$this->ftp_root.$facility_code.'/'.$file_name, 'ascii', 0775);
+
+			$ch = curl_init();
+			$fp = fopen($file_path, 'r');
+			curl_setopt($ch, CURLOPT_URL, 'ftp://ftpuser:ftpuser@commodities.nascop.org/'. $this->ftp_root.$facility_code.'/'.$file_name);
+			curl_setopt($ch, CURLOPT_UPLOAD, 1);
+			curl_setopt($ch, CURLOPT_INFILE, $fp);
+			curl_setopt($ch, CURLOPT_INFILESIZE, filesize($file_path));
+			curl_exec ($ch);
+			$error_no = curl_errno($ch);
+			curl_close ($ch);
+			if ($error_no == 0) {
+				$error = 'File uploaded succesfully.';
+				exit;
+			} else {
+				$error = 'File upload error.';
+				exit;
+			}
 		}
 		else{
 			echo "backup already done";
 		}
 		$this->disconnect_ftp();
 	}
+	public function encrypt_backup($filename){
+		echo "encrypting file ".$filename;
 
+		$handle = fopen($filename, "r");
+		$content = fread($handle, filesize($filename));
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+		$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $content, MCRYPT_MODE_ECB, $iv);
+		// $newfile = "e_".$filename;
+		$newfile = $filename;
+
+		$nfile = fopen($newfile, 'w');
+		fwrite($nfile, $crypttext);
+		fclose($nfile);
+		return true;
+	}
+	public function decrypt_backup($filename){	
+		echo "decrypting file ".$filename;
+		$handle = fopen($filename, "r");
+		$content = fread($handle, filesize($filename));
+
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+		$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+		$decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, $content, MCRYPT_MODE_ECB, $iv);
+		$newfile = 'd_'.substr($filename, 2);
+		$nfile = fopen($newfile, 'w');
+		fwrite($nfile, $decrypttext);
+		fclose($nfile);
+		return true;
+
+	}
 
 
 	public function delete_backup() {
@@ -369,14 +420,13 @@ class Backup extends MX_Controller {
 	}
 
 
-
-	public function zip_backup($file_path = null) {
-		$this->load->library('zip');
+	public function zip_backup($filename = null,$file_path = null) {
 		ini_set('memory_limit', '-1'); 
+		$file_path =  FCPATH.'backup_db/'.$filename;
 
-		$data = $this->zip->read_file($file_path,FALSE);
-		
-		$this->zip->add_data($file_path, $data);
+		$this->load->library('zip');
+
+		$data = $this->zip->read_file($filename,FALSE);	
 		// Write the zip file to a folder on your server. Name it "my_backup.zip"
 		if ($this->zip->archive($file_path.'.zip')){
 			return true;}
@@ -384,6 +434,7 @@ class Backup extends MX_Controller {
 		}
 		public function delete_file($file_path) {
 			if(unlink($file_path)) {
+				$this->zip->clear_data();
 				return true;
 			}
 			else {
@@ -408,8 +459,55 @@ class Backup extends MX_Controller {
 			}
 		}
 
+		/* creates a compressed zip file */
+		function create_zip($files = array(),$destination = '',$overwrite = false) {
+	//if the zip file already exists and overwrite is false, return false
+			if(file_exists($destination) && !$overwrite) { return false; }
+	//vars
+			$valid_files = array();
+	//if files were passed in...
+	// if(is_array($files)) {
+		//cycle through each file
+		// foreach($files as $file) {
+			//make sure the file exists
+			if(file_exists($file)) {
+				$valid_files[] = $file;
+			// }
+			}	
+	// }
+	//if we have good files...
+			if(count($valid_files)) {
+		//create the archive
+				$zip = new ZipArchive();
+				if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+					return false;
+				}
+		//add the files
+				foreach($valid_files as $file) {
+					$zip->addFile($file,$file);
+				}
+		//debug
+		//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
 
-		
+		//close the zip -- done!
+				$zip->close();
+
+		//check to make sure the file exists
+				return file_exists($destination);
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+		function test(){
+			$file_path =  FCPATH.'backup_db';
+			echo $file_path;
+			$this->encrypt_backup($file_path.'/13122_20171120071044_v3.2.3.sql.zip');
+			die;
+		}
 		public function template($data) {
 			$data['show_menu'] = 0;
 			$data['show_sidemenu'] = 0;
