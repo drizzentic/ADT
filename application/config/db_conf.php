e6081ed0d24b310a2f6c77b048511f48492ad900
Kevin Marete <kevomarete@gmail.com>
2017-11-27 15:01:15 +0300

    #Fix: Reset the database name

--- application/config/db_conf.php
+++ application/config/db_conf.php
@@ -3,5 +3,5 @@
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
-$db['default']['database'] = 'testadt_maragua';
+$db['default']['database'] = 'testadt';
 $db['default']['port'] = 3306;
