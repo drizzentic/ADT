d0e491411b80ee3b1e61392b89654a7697349f1f
Nguru <billnguts@gmail.com>
2017-12-22 09:31:09 +0300

    stopped unique to duplicate from running on drugcode.dose

--- assets/migrations/2017_12_15_115100_call_duplicate_to_unique_proc.sql
+++ assets/migrations/2017_12_15_115100_call_duplicate_to_unique_proc.sql
@@ -1,7 +1,6 @@
 CALL `proc_duplicate_to_unique`('testadt', 'temp_regimen_category', 'regimen_category', 'name', 'regimen', 'category')//
 CALL `proc_duplicate_to_unique`('testadt', 'temp_drug_classification', 'drug_classification', 'name', 'drugcode', 'classification')//
 CALL `proc_duplicate_to_unique`('testadt', 'temp_drug_unit', 'drug_unit', 'name', 'drugcode', 'unit')//
-CALL `proc_duplicate_to_unique`('testadt', 'temp_dose', 'dose', 'name', 'drugcode', 'dose')//
 CALL `proc_duplicate_to_unique`('testadt', 'temp_drug_instructions', 'drug_instructions', 'name', 'drugcode', 'instructions')//
 CALL `proc_duplicate_to_unique`('testadt', 'temp_generic_name', 'generic_name', 'name', 'drugcode', 'generic_name')//
 CALL `proc_duplicate_to_unique`('testadt', 'temp_prep_reason', 'prep_reason', 'name', 'patient_prep_test', 'prep_reason_id')//
