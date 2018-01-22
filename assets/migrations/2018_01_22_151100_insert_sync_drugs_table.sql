37b06d5306fbf9eb0725cbf00a7053177666946b
Nguru <billnguts@gmail.com>
2018-01-22 15:12:26 +0300

    added missing sync drugs [from alfie]

---
+++ assets/migrations/2018_01_22_151100_insert_sync_drugs_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO `sync_drug` ( `name`, `abbreviation`, `strength`, `packsize`, `formulation`, `unit`, `note`, `weight`, `category_id`, `regimen_id`, `Active`) VALUES
+('Nevirapine','NVP','10mg/ml',100,'Suspension',NULL,NULL,999,2,0,'1'),
+('Nevirapine','NVP','10mg/ml',240,'Suspension',NULL,NULL,999,2,0,'1'),
+('Nevirapine','NVP','10mg/ml',60,'Suspension',NULL,NULL,999,2,0,'1'),
+('Lopinavir/Ritonavir ','LPV/r','100/25mg',60,'Tablet',NULL,NULL,999,2,0,'1')//
