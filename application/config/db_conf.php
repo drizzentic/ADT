0b2947d28488c49b494f11ef5edea398d1f05470
Kevin Marete <kevomarete@gmail.com>
2018-02-06 14:59:18 +0300

    #Added: Changed default mysql port

--- application/config/db_conf.php
+++ application/config/db_conf.php
@@ -4,4 +4,4 @@ $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'testadt';
-$db['default']['port'] = 3306;
+$db['default']['port'] = 3307;
