2ed61be65883537b629f4767ae84b41687ca86cf
Kevin Marete <kevomarete@gmail.com>
2018-02-07 20:17:44 +0300

    #Added: Beta Tag for testing

--- application/config/adt_config.php
+++ application/config/adt_config.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['adt_version'] = '3.3';
+$config['adt_version'] = '3.3(Beta)';
 
 /* End of file config.php */
 /* Location: ./application/config/adt_config.php */
