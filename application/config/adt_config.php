fa1a93c9ec288434cb00bc89a9c6d55bd47333a6
Nguru <billnguts@gmail.com>
2018-02-12 19:25:51 +0300

    ADT version 3.3 release! (2)

--- application/config/adt_config.php
+++ application/config/adt_config.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['adt_version'] = '3.3(Beta)';
+$config['adt_version'] = '3.3';
 
 /* End of file config.php */
 /* Location: ./application/config/adt_config.php */
--- tools/application/config/config.php
+++ tools/application/config/config.php
@@ -499,4 +499,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-$config['adt_version'] = '3.3(Beta)';
+$config['adt_version'] = '3.3';
