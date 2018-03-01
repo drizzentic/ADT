d8006c53351952efea5a32ce55752dc94da7c44c
Nguru <billnguts@gmail.com>
2018-03-01 14:29:17 +0300

    differentiated care sql fix

--- application/controllers/report_management.php
+++ application/controllers/report_management.php
@@ -3258,10 +3258,11 @@ public function getScheduledPatients($from = "", $to = "", $filter_from = NULL, 
 			//Get all patients who have apppointments on the selected date range
 		$sql = "SELECT pa.patient,pa.appointment ,ca.appointment as clinic_appointment,
 				CASE
-				WHEN  ca.differentiated_care = 1 THEN 'YES' ELSE  'NO' END as diff_care,
+				WHEN  p.differentiated_care = 1 THEN 'YES' ELSE  'NO' END as diff_care,
 				DATEDIFF(ca.appointment, pa.appointment) as days_diff
 				FROM patient_appointment pa
 				LEFT JOIN clinic_appointment ca on ca.id = pa.clinical_appointment
+				LEFT JOIN patient p on p.patient_number_ccc = pa.patient
 				WHERE pa.appointment BETWEEN '$from' AND '$to' 
 				AND pa.facility='$facility_code' 
 				GROUP BY patient,appointment";
