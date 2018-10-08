ad3b12a201d4dd8d0e4d776c9814c39023046943
Kevin Marete <kevomarete@gmail.com>
2018-10-08 11:51:09 +0300

    #Fixed: default db config #Added: latest adminer file

--- application/config/db_conf.php
+++ application/config/db_conf.php
@@ -2,6 +2,6 @@
 <?php 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'testadt2';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'testadt';
 $db['default']['port'] = 3306;
