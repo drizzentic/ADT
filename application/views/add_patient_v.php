3f7187f8a5f49fb6c57f37fc3ac2cdf451297406
William Nguru <billnguts@gmail.com>
2019-01-21 09:59:40 +0300

    [fix] maxlenght to patientccc number on add/edit

--- application/views/add_patient_v.php
+++ application/views/add_patient_v.php
@@ -611,7 +611,7 @@
 							</div>
 							<div class="mid-row">
 								<label> <span class='astericks'>*</span>Patient Number CCC </label>
-								<input type="text"name="patient_number" id="patient_number" class="validate[required]" value="<?=$facility_code?>-">
+								<input type="text"name="patient_number" id="patient_number" class="validate[required]" value="<?=$facility_code?>-" maxlength="11">
 							</div>
 						</div>
 						<div class="max-row">
--- application/views/edit_patients_v.php
+++ application/views/edit_patients_v.php
@@ -996,7 +996,7 @@ function getAge(dateString) {
 								</div>
 								<div class="mid-row">
 									<label> <span class='astericks'>*</span>Patient Number CCC </label>
-									<input type="text" name="patient_number" id="patient_number" class="validate[required]">
+									<input type="text" name="patient_number" id="patient_number" class="validate[required]" maxlength="11">
 									<input type="hidden" name="original_patient_number" id="original_patient_number">
 								</div>
 							</div>
