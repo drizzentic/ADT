4815bc67b6f9136a7cd9a3d2eb83e7a1bd55e4c9
Nguru <billnguts@gmail.com>
2019-06-10 14:59:04 +0300

    [fix] most common ADT config setting

--- application/config/db_conf.php
+++ application/config/db_conf.php
@@ -1,7 +1,6 @@
-
 <?php 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'root';
 $db['default']['database'] = 'testadt';
-$db['default']['port'] = 3306;
+$db['default']['port'] = 3307;
