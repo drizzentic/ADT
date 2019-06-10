b600ee353aa61291726af366b1601f93f2f7a73c
Nguru <billnguts@gmail.com>
2019-06-10 17:51:35 +0300

    [fix] auto populate ccc on click service

--- application/views/edit_patients_v.php
+++ application/views/edit_patients_v.php
@@ -34,20 +34,24 @@ foreach($results as $result){
 	<script type="text/javascript">
 			function setCCC(service) {
 				if (service=='ART'){
+					$('#patient_number').val('<?=$facility_code. $cs;?>');
 					$('#patient_number').attr('maxlength','11');
 				}
 				if (service=='PREP'){
+					$('#patient_number').val('<?=$facility_code. $cs.date('Y').$cs;?>');
 					$('#patient_number').attr('maxlength','16');
 				}
 				if (service=='PEP'){
+					$('#patient_number').val('PEP<?=$cs.$facility_code. $cs;?>');
 					$('#patient_number').attr('maxlength','15');
 				}
-
 				if (service=='HEP'){
+					$('#patient_number').val('HEP<?=$cs.$facility_code. $cs;?>');
 					$('#patient_number').attr('maxlength','15');
 				}
 
 				if (service=='HEI'){
+					$('#patient_number').val('<?=$facility_code. $cs.date('Y').$cs;?>');
 					$('#patient_number').attr('maxlength','16');
 				}			
 			}
